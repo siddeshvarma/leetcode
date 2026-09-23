4187d46a3cacdcd15ef28a6d963e2beeb2968e7e
siddeshvarma <153838208+siddeshvarma@users.noreply.github.com>
2026-09-23 22:26:29 +0530

    Time: 3 ms (93.04%) | Memory: 45.3 MB (35.78%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans=new int[10001];
+        Stack<Integer>st=new Stack<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<nums2[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[nums2[i]]=-1;
+            }
+            else{
+                ans[nums2[i]]=st.peek();
+            }
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.length;i++){
+            nums1[i]=ans[nums1[i]];
+        }return nums1;
+        
+    }
+}
